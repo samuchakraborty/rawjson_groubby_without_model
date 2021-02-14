fa401ff3b07f3948bbb9a04d57b62e8be4604f1f
samuchakraborty <samuckb@gmail.com>
2021-02-14 10:27:42 +0600

    completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import "package:collection/collection.dart";
 import 'network.dart';
 
 void main() {
@@ -15,18 +15,164 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     // TODO: implement initState
-    Network().getCart();
+    //  Network().getCart();
 
     super.initState();
   }
 
+  List key = [];
+  var sellerWiseProduct = [];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-      child: Container(
-        child: Text("hello world!"),
-      ),
-    ));
+    return SafeArea(
+      child: Scaffold(
+          body: FutureBuilder(
+        future: Network().getCart(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return Container();
+          } else {
+            var newMap;
+            newMap = groupBy(snapshot.data['data']['cart_items'].listObject,
+                (obj) => obj['product']['seller']['shop_name']);
+
+            List sellerShopName = [];
+
+            for (var i = 0;
+                i < snapshot.data['data']['cart_items'].listObject.length;
+                i++) {
+              sellerShopName.add(snapshot.data['data']['cart_items'][i]
+                  ['product']['seller']['shop_name']);
+            }
+            var distinctIds = sellerShopName.toSet().toList();
+            //  print(newMap[distinctIds[1]][1]['is_anaz_empire']);
+
+            for (var i = 0; i < distinctIds.length; i++) {
+              key.add(distinctIds[i].rawString());
+
+              for (var j = 0; j < newMap[distinctIds[i]].length; j++) {
+                sellerWiseProduct
+                    .add(newMap[distinctIds[i]][j]['product']['name']);
+              }
+            }
+
+            return ListView(
+              shrinkWrap: true,
+              children: [
+                Column(
+                  children: [
+                    ListView.builder(
+                      itemCount: key.length,
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            Card(
+
+                              color: Colors.blue,
+                                child: Text(key[index]),),
+                            ListView.builder(
+                              physics: NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: newMap[distinctIds[index]].length,
+                              itemBuilder: (context, i) {
+                                return Column(
+                                  children: [
+                                    Card(
+                                      color: Colors.red,
+                                      child: Column(
+                                        children: [
+                                          Container(
+                                            margin: EdgeInsets.only(left: 10),
+                                            height: 20,
+                                            alignment: Alignment.topLeft,
+                                            child: Text("Item No: " + (i + 1).toString()
+                                              //  + (i + 1).toString()
+                                            ),
+                                          ),
+                                          Text(newMap[distinctIds[index]][i]
+                                              ['product']['name']),
+
+Container(
+  height: 120,
+
+  child:   Image.network("https://anazbd.com/"+newMap[distinctIds[index]][i]
+
+                                  ['product']['feature_image'] ),
+),
+
+                                        ],
+                                      ),
+                                    ),
+
+                                    // reuseAbleCard(
+                                    //     index: i,
+                                    //     context: context,
+                                    //     myItems: sellerWiseProduct),
+                                  ],
+                                );
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            );
+          }
+        },
+      )),
+    );
   }
+}
+
+Widget reuseAbleCard({int index, BuildContext context, myItems, myItem}) {
+  return Column(
+    children: [
+      Card(
+        elevation: 4,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 7,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 10),
+              height: 20,
+              alignment: Alignment.topLeft,
+              child: Text("Item No: " + (index + 1).toString()
+                  //  + (i + 1).toString()
+                  ),
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(width: 3),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width / 2,
+                        height: 30,
+                        child: Text(
+                          myItems[index],
+                          // overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    ],
+                  ),
+                ]),
+          ],
+        ),
+      )
+    ],
+  );
 }
--- lib/network.dart
+++ lib/network.dart
@@ -7,138 +7,193 @@ class Network {
     final jsonResponse = await rootBundle.loadString("assets/data.json");
 
     final j = JSON.parse(jsonResponse);
-   // final _id = j['data'];
-    final id = j['data']['cart_items'];
-
-    List anazEmpire = [];
-    List anazSpotLights = [];
-    List other = [];
-
-    ///
-    for (var i = 0; i < id.listObject.length; i++) {
-      // ignore: unrelated_type_equality_checks
-      var anazempire = j['data']['cart_items'][i]['is_anaz_empire'];
-      // ignore: unrelated_type_equality_checks
-      if (anazempire == 1) {
-        //   anazEmpire.add(j['data']['cart_items'][i]);
-
-        //print("anaz_empire =" + i.toString());
-        // ignore: unrelated_type_equality_checks
-      } else if (anazempire == 0) {
-        //   anazSpotLights.add(j['data']['cart_items'][i]);
-        //print("anaz Spotlight=" + i.toString());
-      } else {
-        other.add(j['data']['cart_items'][i]);
-      }
-    }
-
-    // print(anazSpotLights.length);
-    // print(anazEmpire.length);
-    // anazEmpire
-    //     .forEach((element) => print("anazEmpire   " + element.toString()));
-    // anazSpotLights
-    //     .forEach((element) => print("anazSpotLights   " + element.toString()));
-    var newMap;
-    newMap = groupBy(j['data']['cart_items'].listObject,
-        (obj) => obj['product']['seller']['shop_name']);
-    //   streetsList = new List<String>.from(newMap);
-    // print("anaz Groceries");
-    // print(newMap['Anaz Groceries']);
-
-//     print("anaz ANAZ MALL");
-//     print(newMap.length);
-// print(newMap.isEmpty);
-    List sellerShopName = [];
-
-    for (var i = 0; i < j['data']['cart_items'].listObject.length; i++) {
-      sellerShopName
-          .add(j['data']['cart_items'][i]['product']['seller']['shop_name']);
-    }
-    var distinctIds = sellerShopName.toSet().toList();
-    // print(distinctIds);
-
-    for (var i = 0; i < distinctIds.length; i++) {
-      //  print(distinctIds[i].rawString());
-
-      for (var j = 0; j < newMap[distinctIds[i]].length; j++) {
-        //print(newMap[distinctIds[i]][j]['product']['name']);
-      }
-    }
-
-    // for (var i = 0; i < id.listObject.length; i++) {
-    //   // ignore: unrelated_type_equality_checks
-    //   var anaemic = j['data']['cart_items'][i]['is_anaz_empire'];
-    //   // ignore: unrelated_type_equality_checks
-    //   if (anaemic == 1) {
-    //     print("Anaz Empire");
-    //     for (var i = 0; i < distinctIds.length; i++) {
-    //       print(distinctIds[i].rawString());
-    //
-    //       for (var j = 0; j < newMap[distinctIds[i]].length; j++) {
-    //         print(newMap[distinctIds[i]][j]['product']['name']);
-    //       }
-    //     }
-    //
-    //   } else if (anaemic == 0) {
-    //     print("Anaz Spotlight");
-    //     for (var i = 0; i < distinctIds.length; i++) {
-    //       print(distinctIds[i].rawString());
-    //
-    //       for (var j = 0; j < newMap[distinctIds[i]].length; j++) {
-    //         print(newMap[distinctIds[i]][j]['product']['name']);
-    //       }
-    //     }
-    //
-    //   } else {
-    //     print("others");
-    //     for (var i = 0; i < distinctIds.length; i++) {
-    //       print(distinctIds[i].rawString());
-    //
-    //       for (var j = 0; j < newMap[distinctIds[i]].length; j++) {
-    //         print(newMap[distinctIds[i]][j]['product']['name']);
-    //       }
-    //     }
-    //
-    //   }
-    // }
-    //print(newMap[distinctIds[1]][1]['is_anaz_spotlight']);
-    for (var i = 0; i < distinctIds.length; i++) {
-      //  print(distinctIds[i].rawString());
-
-      for (var j = 0; j < newMap[distinctIds[i]].length; j++) {
-        if (newMap[distinctIds[i]][j]['is_anaz_spotlight'] == 1) {
-          //  print("is_anaz_empire");
-          //  print(newMap[distinctIds[i]][j]['product']['name']);
-          anazEmpire.add(newMap[distinctIds[i]][j]['product']['name']);
-        } else {
-          //  print("anazSpotLights");
-          //  print(newMap[distinctIds[i]][j]['product']['name']);
-          anazSpotLights.add(newMap[distinctIds[i]][j]['product']['name']);
-        }
-      }
-    }
-
-    // var anaz = anazSpotLights.toSet().toList();
-    // print(anaz[1]);
-    // print(anazSpotLights);
-    //  print(anazEmpire);
-    // print(anaz.toList());
-
-    //  anazEmpire.forEach((element) => print("anazEmpire   " + element.toString()));
-    anazSpotLights
-        .forEach((element) => print("AnazSpotLights " + element.toString()));
-
-    // if (newMap[distinctIds[i]][j]['is_anaz_empire'] == 0) {
-    //   //print("Anaz empire");
-    //   print(newMap[distinctIds[i]][j]['product']['name']);
-    // } else if (newMap[distinctIds[i]][j]['is_anaz_empire'] == 1) {
-    //   //print("Anaz SpotLight");
-    //   print(newMap[distinctIds[i]][j]['product']['name']);
-    // }
-
-    // for (var j = 0; j < newMap[distinctIds[0]].length; j++) {
-    //   // print(distinctIds[i]);
-    //   print(newMap[sellerShopName[0]][j]['product']['name']);
-    // }
+    return JSON.parse(jsonResponse);
   }
 }
+//    // final _id = j['data'];
+//     final id = j['data']['cart_items'];
+//
+//     List anazEmpire = [];
+//     List anazSpotLights = [];
+//     List other = [];
+//
+//     ///
+//     for (var i = 0; i < id.listObject.length; i++) {
+//       // ignore: unrelated_type_equality_checks
+//       var anazempire = j['data']['cart_items'][i]['is_anaz_empire'];
+//       // ignore: unrelated_type_equality_checks
+//       if (anazempire == 1) {
+//         //   anazEmpire.add(j['data']['cart_items'][i]);
+//
+//         //print("anaz_empire =" + i.toString());
+//         // ignore: unrelated_type_equality_checks
+//       } else if (anazempire == 0) {
+//         //   anazSpotLights.add(j['data']['cart_items'][i]);
+//         //print("anaz Spotlight=" + i.toString());
+//       } else {
+//         other.add(j['data']['cart_items'][i]);
+//       }
+//     }
+//
+//     // print(anazSpotLights.length);
+//     // print(anazEmpire.length);
+//     // anazEmpire
+//     //     .forEach((element) => print("anazEmpire   " + element.toString()));
+//     // anazSpotLights
+//     //     .forEach((element) => print("anazSpotLights   " + element.toString()));
+//     var newMap;
+//     newMap = groupBy(j['data']['cart_items'].listObject,
+//         (obj) => obj['product']['seller']['shop_name']);
+//     //   streetsList = new List<String>.from(newMap);
+//     // print("anaz Groceries");
+//     // print(newMap['Anaz Groceries']);
+//
+// //     print("anaz ANAZ MALL");
+// //     print(newMap.length);
+// // print(newMap.isEmpty);
+//     List sellerShopName = [];
+//
+//     for (var i = 0; i < j['data']['cart_items'].listObject.length; i++) {
+//       sellerShopName
+//           .add(j['data']['cart_items'][i]['product']['seller']['shop_name']);
+//     }
+//     var distinctIds = sellerShopName.toSet().toList();
+//   print(newMap[distinctIds[1]][1]['is_anaz_empire']);
+//
+//     for (var i = 0; i < distinctIds.length; i++) {
+//       print(distinctIds[i].rawString());
+//
+//       for (var j = 0; j < newMap[distinctIds[i]].length; j++) {
+//        print(newMap[distinctIds[i]][j]['product']['name']);
+//       }
+//     }
+//
+//
+//   }
+// }
+
+class NEW{
+
+
+
+  // for (var i = 0; i < id.listObject.length; i++) {
+  //   // ignore: unrelated_type_equality_checks
+  //   var anaemic = j['data']['cart_items'][i]['is_anaz_empire'];
+  //   // ignore: unrelated_type_equality_checks
+  //   if (anaemic == 1) {
+  //     print("Anaz Empire");
+  //     for (var i = 0; i < distinctIds.length; i++) {
+  //       print(distinctIds[i].rawString());
+  //
+  //       for (var j = 0; j < newMap[distinctIds[i]].length; j++) {
+  //         print(newMap[distinctIds[i]][j]['product']['name']);
+  //       }
+  //     }
+  //
+  //   } else if (anaemic == 0) {
+  //     print("Anaz Spotlight");
+  //     for (var i = 0; i < distinctIds.length; i++) {
+  //       print(distinctIds[i].rawString());
+  //
+  //       for (var j = 0; j < newMap[distinctIds[i]].length; j++) {
+  //         print(newMap[distinctIds[i]][j]['product']['name']);
+  //       }
+  //     }
+  //
+  //   } else {
+  //     print("others");
+  //     for (var i = 0; i < distinctIds.length; i++) {
+  //       print(distinctIds[i].rawString());
+  //
+  //       for (var j = 0; j < newMap[distinctIds[i]].length; j++) {
+  //         print(newMap[distinctIds[i]][j]['product']['name']);
+  //       }
+  //     }
+  //
+  //   }
+  // }
+  //print(newMap[distinctIds[1]][1]['is_anaz_spotlight']);
+  // for (var i = 0; i < distinctIds.length; i++) {
+  //   //  print(distinctIds[i].rawString());
+  //
+  //   for (var j = 0; j < newMap[distinctIds[i]].length; j++) {
+  //     //print(distinctIds[i].rawString());
+  //     if (newMap[distinctIds[i]][j]['is_anaz_spotlight'] == 0) {
+  //       //  print("is_anaz_empire");
+  //       //  print(newMap[distinctIds[i]][j]['product']['name']);
+  //       //print('anazEmpire');
+  //
+  //      // print(newMap[distinctIds[i]][j]['product']['name']);
+  //       anazEmpire.add(newMap[distinctIds[i]][j]['product']['name']);
+  //     } else {
+  //       //  print("anazSpotLights");
+  //       //  print(newMap[distinctIds[i]][j]['product']['name']);
+  //       anazSpotLights.add(newMap[distinctIds[i]][j]['product']['name']);
+  //     }
+  //   }
+  // }
+
+
+  // for (var i = 0; i < id.listObject.length; i++) {
+  //   // ignore: unrelated_type_equality_checks
+  //   var anazempire = j['data']['cart_items'][i]['is_anaz_empire'];
+  //   // ignore: unrelated_type_equality_checks
+  //   if (anazempire == 0) {
+  //     print("anaz_empire =");
+  //     //   anazEmpire.add(j['data']['cart_items'][i]);
+  //     for (var i = 0; i < distinctIds.length; i++) {
+  //       print(distinctIds[i].rawString());
+  //
+  //       for (var j = 0; j < newMap[distinctIds[i]].length; j++) {
+  //         if(newMap[distinctIds[i]][j]['product']['is_anaz_empire'] == 0 ){
+  //         print(newMap[distinctIds[i]][j]['product']['name']);}
+  //       }
+  //     }
+  //
+  //     //print("anaz_empire =" + i.toString());
+  //     // ignore: unrelated_type_equality_checks
+  //   } else if (anazempire == 0) {
+  //     //   anazSpotLights.add(j['data']['cart_items'][i]);
+  //     //print("anaz Spotlight=" + i.toString());
+  //   } else {
+  //    // other.add(j['data']['cart_items'][i]);
+  //   }
+  // }
+
+  // var anaz = anazSpotLights.toSet().toList();
+  // print(anaz[1]);
+  // print(anazSpotLights);
+  //  print(anazEmpire);
+  // print(anaz.toList());
+
+  //anazEmpire.forEach((element) => print("AnazEmpire => " + element.toString()));
+  // anazSpotLights
+  //    .forEach((element) => print("AnazSpotLights => " + element.toString()));
+//
+  // if (newMap[distinctIds[i]][j]['is_anaz_empire'] == 0) {
+  //   //print("Anaz empire");
+  //   print(newMap[distinctIds[i]][j]['product']['name']);
+  // } else if (newMap[distinctIds[i]][j]['is_anaz_empire'] == 1) {
+  //   //print("Anaz SpotLight");
+  //   print(newMap[distinctIds[i]][j]['product']['name']);
+  // }
+
+  // for (var j = 0; j < newMap[distinctIds[0]].length; j++) {
+  //   // print(distinctIds[i]);
+  //   print(newMap[sellerShopName[0]][j]['product']['name']);
+  // }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
